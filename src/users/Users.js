4656d5fe14a7381074dcd87415f5e7ab1ca9e76d
zhaoou <zhaoouzhao@gmail.com>
2019-03-01 16:06:18 -0600

    user search

--- src/users/Users.js
+++ src/users/Users.js
@@ -24,8 +24,9 @@ class Users extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {users: [{id: Math.random() + "_", email: "zhaoouzhao@aliyun.com", name: "michael"}]};
+        this.state = {users: [{id: Math.random() + "_", email: "zhaoouzhao@aliyun.com", name: "michael"}], keyword:"", found:[]};
         this.add = this.add.bind(this)
+        this.handleTyping = this.handleTyping.bind(this);
     }
 
     add({id = Math.random() + "_", name, email}) {
@@ -41,8 +42,16 @@ class Users extends Component {
         this.setState({users: oldUsers});
     }
 
+    handleTyping(event) {
+        let input= event.target.value;
+        this.setState(() => {
+            let newUsers = [...this.state.users.filter((user)=> user.name.includes(input) || user.email.includes(input))];
+            return {users: [...this.state.users], keyword: input, found: newUsers };
+        });
+    }
+
     componentDidMount() {
-        UserAPI.getAll().then( (users) => { this.setState({ users }) } )
+        UserAPI.getAll().then( (users) => { this.setState({ users: users, found:users }) } )
     }
 
     render() {
@@ -56,11 +65,12 @@ class Users extends Component {
                     </div>
 
                     <div className="col">
+                        <input type="text" name="search" value = {this.state.keyword} onChange={this.handleTyping}/>
                         <ul>
-                            {this.state.users.map((e) => (
+                            {this.state.found.map((e) => (
                                 <li>
                                     <Link to={`/user/edit/${e.id}`}>
-                                        {e.name.toString()} {e.email}
+                                        {e.name.toString()} {" "}  {e.email}
                                     </Link>
 
                                 </li>
